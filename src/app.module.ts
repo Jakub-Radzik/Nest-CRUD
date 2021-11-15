44a596dcba49845d3d4b1c810294a7e9dc407876
Jakub <jradzik4@gmail.com>
2021-11-15 19:46:10 +0100

    auto generated CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { PersonsModule } from './persons/persons.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProductsModule, PersonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
